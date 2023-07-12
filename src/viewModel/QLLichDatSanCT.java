5d6f770c59a5737492d34c479722c353902bb5f8
Quan-Lew <quanlh.jp@gmail.com>
2023-07-12 15:37:51 +0700

    new viewModel

---
+++ src/viewModel/QLLichDatSanCT.java
@@ -0,0 +1,119 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package viewModel;
+
+import java.sql.Time;
+import java.util.Date;
+
+/**
+ *
+ * @author Quan Lew
+ */
+public class QLLichDatSanCT {
+     private String maDS;
+    private String tenSan;
+    private Integer loai;
+    private String tenKh;
+    private String sdt;
+    private Time gioBD;
+    private Time gioKT;
+    private Date ngay;
+    private Integer trangThai;
+
+    public QLLichDatSanCT() {
+    }
+
+    public QLLichDatSanCT(String maDS, String tenSan, Integer loai, String tenKh, String sdt, Time gioBD, Time gioKT, Date ngay, Integer trangThai) {
+        this.maDS = maDS;
+        this.tenSan = tenSan;
+        this.loai = loai;
+        this.tenKh = tenKh;
+        this.sdt = sdt;
+        this.gioBD = gioBD;
+        this.gioKT = gioKT;
+        this.ngay = ngay;
+        this.trangThai = trangThai;
+    }
+
+    public String getMaDS() {
+        return maDS;
+    }
+
+    public void setMaDS(String maDS) {
+        this.maDS = maDS;
+    }
+
+    public String getTenSan() {
+        return tenSan;
+    }
+
+    public void setTenSan(String tenSan) {
+        this.tenSan = tenSan;
+    }
+
+    public Integer getLoai() {
+        return loai;
+    }
+
+    public void setLoai(Integer loai) {
+        this.loai = loai;
+    }
+
+    public String getTenKh() {
+        return tenKh;
+    }
+
+    public void setTenKh(String tenKh) {
+        this.tenKh = tenKh;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+
+    public Time getGioBD() {
+        return gioBD;
+    }
+
+    public void setGioBD(Time gioBD) {
+        this.gioBD = gioBD;
+    }
+
+    public Time getGioKT() {
+        return gioKT;
+    }
+
+    public void setGioKT(Time gioKT) {
+        this.gioKT = gioKT;
+    }
+
+    public Date getNgay() {
+        return ngay;
+    }
+
+    public void setNgay(Date ngay) {
+        this.ngay = ngay;
+    }
+
+    public Integer getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(Integer trangThai) {
+        this.trangThai = trangThai;
+    }
+
+    @Override
+    public String toString() {
+        return "VmLichDatSanCT{" + "maDS=" + maDS + ", tenSan=" + tenSan + ", loai=" + loai + ", tenKh=" + tenKh + ", sdt=" + sdt + ", gioBD=" + gioBD + ", gioKT=" + gioKT + ", ngay=" + ngay + ", trangThai=" + trangThai + '}';
+    }
+    
+    
+    
+}
---
+++ src/viewModel/QLTaiKhoan.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package viewModel;
+
+/**
+ *
+ * @author Quan Lew
+ */
+public class QLTaiKhoan {
+      private String id;
+    private String tenDangNhap;
+    private String matKhau;
+    private String email;
+    private String sdt;
+    private String idcv;
+
+    public QLTaiKhoan() {
+    }
+
+    public QLTaiKhoan(String id, String tenDangNhap, String matKhau, String email, String sdt, String idcv) {
+        this.id = id;
+        this.tenDangNhap = tenDangNhap;
+        this.matKhau = matKhau;
+        this.email = email;
+        this.sdt = sdt;
+        this.idcv = idcv;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTenDangNhap() {
+        return tenDangNhap;
+    }
+
+    public void setTenDangNhap(String tenDangNhap) {
+        this.tenDangNhap = tenDangNhap;
+    }
+
+    public String getMatKhau() {
+        return matKhau;
+    }
+
+    public void setMatKhau(String matKhau) {
+        this.matKhau = matKhau;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+
+    public String getIdcv() {
+        return idcv;
+    }
+
+    public void setIdcv(String idcv) {
+        this.idcv = idcv;
+    }
+
+    @Override
+    public String toString() {
+        return "VmTaiKhoan{" + "id=" + id + ", tenDangNhap=" + tenDangNhap + ", matKhau=" + matKhau + ", email=" + email + ", sdt=" + sdt + ", idcv=" + idcv + '}';
+    }
+    
+    
+    
+}
